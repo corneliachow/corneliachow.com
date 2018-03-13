6e983368d2fbaedae11163456cf010cb7f07dafd
Cornelia Chow <cornelia.chow@gmail.com>
2018-03-12 23:08:53 -0500

    Home page edits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
 }
 
 function closeNav() {
