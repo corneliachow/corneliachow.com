b829e3671225dd93796f41f0bf5b80a18b55a6a3
Cornelia Chow <cornelia.chow@gmail.com>
2018-04-19 17:46:33 -0400

    clean slate, implemented scroll progress border

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function openNav() {
-    $("#sidenav").css("width", "200px");
-    $("#main").css("margin-left", "200px");
-}
 
-function closeNav() {
-    $("#sidenav").css("width", "0");
-    $("#main").css("margin-left", "0");
-}
+// progress scroll border
+$(document).on("scroll", function () {
+  // total border length
+  var borderLen = ($(window).height() * 2) + ($(window).width() * 2);
+
+  // percentage of current scrolled position
+  var percentScrolled = $(document).scrollTop() / ($(document).height() - $(window).height());
+
+  // ratio of horizontal border to total border
+  var horizontal = $(window).width() / borderLen;
+
+  // ratio of vertical border to total border
+  var vertical = $(window).height() / borderLen;
+
+  var bordertop = percentScrolled / horizontal;
+  var borderright = (percentScrolled - horizontal) / vertical;
+  var borderbottom = (percentScrolled - horizontal - vertical) / horizontal;
+  var borderleft = (percentScrolled - horizontal - vertical - horizontal) / vertical;
+
+  $("div.border-top").css("width",  100 * bordertop + "%");
+  $("div.border-right").css("height", 100 * borderright + "%");
+  $("div.border-bottom").css("width",  100 * borderbottom + "%");
+  $("div.border-left").css("height", 100 * borderleft + "%");
+})
